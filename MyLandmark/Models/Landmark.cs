8380355b7863501088d15b95c1c4376628fe40f9
hardhill <hardhill@mail.ru>
2017-04-24 00:44:49 +0800

    first commit

---
+++ MyLandmark/Models/Landmark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLandmark.Models
+{
+    public class Landmark
+    {
+        public Landmark()
+        {
+            this.Longitude = 0D;
+            this.IsPrivate = false;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public bool IsPrivate { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+    }
+}
---
+++ MyLandmark/Models/MLMContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyLandmark.Models
+{
+    public class MLMContext:DbContext
+    {
+        DbSet<Landmark> Landmarks { get; set;}
+    }
+}
